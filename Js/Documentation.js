7882181d9b30d03a66e571685e920d09dc2a4579
Vasilis Datseris <iekdatseris@gmail.com>
2022-01-10 01:58:19 +0200

    Game Added

--- Js/Documentation.js
+++ Js/Documentation.js
@@ -1,5 +1,5 @@
-let PageCounter = 0; // Starting Page
-let maxCounter = 5; //Max Pages of Document
+let PageCounter = 1; // Starting Page
+let maxCounter = 3; //Max Pages of Document
 
 document.getElementById('PreviousButton').style.visibility = "hidden";
 //Navigation Buttons
@@ -7,17 +7,15 @@ document.getElementById('PreviousButton').style.visibility = "hidden";
 function PreviousClicked() {
     document.getElementById('NextButton').style.visibility = "visible";
 
-    console.log('previous');
     PageCounter--;
 
-    if(PageCounter === maxCounter)
+    if(PageCounter === 1)
         document.getElementById('PreviousButton').style.visibility = "hidden";
 }
 
 function NextClicked() {
     document.getElementById('PreviousButton').style.visibility = "visible";
-    console.log('previous');
-
+    
     PageCounter++;
 
     if(PageCounter === maxCounter)
@@ -29,10 +27,10 @@ function ContentLoad(id) {
     let content;
 
     if (id === "Intro") {
-        content = $.get("Content/Intro.Html", (data) => {
+        content = $.get("../Pages/Content/Intro.Html", (data) => {
             $(".ContentLoader").html(data);
         })
     }
-
+    
     $(".ContentLoader").html(`<div>${content}</div>`);
 }
