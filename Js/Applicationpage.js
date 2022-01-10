ebefe89c387e373e4e5586d17ba0084ae9fd38fc
Vasilis Datseris <iekdatseris@gmail.com>
2022-01-10 02:59:56 +0200

    Created Application Page

---
+++ Js/Applicationpage.js
@@ -0,0 +1,18 @@
+import lottieWeb from 'https://cdn.skypack.dev/lottie-web';
+
+//Create Animation for Div
+let animDiv = document.getElementsByClassName('Animation')[0];
+let animation = lottieWeb.loadAnimation({
+    container: animDiv,
+    path: '../Images/LoadStats.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    name: "Stats Loading..",
+});
+
+animation.goToAndStop(160,true);
+
+function showStats() {
+
+}
--- Js/Documentation.js
+++ Js/Documentation.js
@@ -8,6 +8,7 @@ function PreviousClicked() {
     document.getElementById('NextButton').style.visibility = "visible";
 
     PageCounter--;
+    setPage(PageCounter);
 
     if(PageCounter === 1)
         document.getElementById('PreviousButton').style.visibility = "hidden";
@@ -17,20 +18,52 @@ function NextClicked() {
     document.getElementById('PreviousButton').style.visibility = "visible";
     
     PageCounter++;
+    setPage(PageCounter);
 
     if(PageCounter === maxCounter)
         document.getElementById('NextButton').style.visibility = "hidden";
 }
 
-//Content load
+//Set Pagination
+function setPage (counter) {
+    let direction;
+
+    switch(counter){
+        case 1 :
+            direction = "Intro"
+            break
+        case 2 :
+            direction = "PWA"
+            break
+        case 3 :
+            direction = "Project"
+            break
+        default :
+            break
+    }
+
+    ContentLoad(direction);
+}
+
+//Content load (Add Static Routing here)
 function ContentLoad(id) {
     let content;
 
-    if (id === "Intro") {
+    if (id === "Intro") 
         content = $.get("../Pages/Content/Intro.Html", (data) => {
             $(".ContentLoader").html(data);
+            PageCounter = 1;
         })
-    }
-    
+    else if (id === "PWA")
+        content = $.get("../Pages/Content/PWA.Html", (data) => {
+            $(".ContentLoader").html(data);
+            PageCounter = 2;
+        })
+    else if (id === "Project")
+        content = $.get("../Pages/Content/Project.Html", (data) => {
+            $(".ContentLoader").html(data);
+            PageCounter = 3;
+        })
+
     $(".ContentLoader").html(`<div>${content}</div>`);
 }
